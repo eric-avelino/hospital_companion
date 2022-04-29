f73d5f3d78a1661452e90f68bb9be4e15a1b2e72
Eric Avelino <eric.avelino08@gmail.com>
2022-04-29 13:13:26 -0300

    pinto2

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, View, Image } from 'react-native';
+// import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
 
+const PaginaInicial = () => {
+	const pressConvidado = () => {
+	    };  
+    return (
+        <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require('./assets/logo.png')} />
+              <Button 
+          title='Entrar'
+          color='#1b4'
+          />
+        <Text 
+          style={styles.text}
+          onPress={pressConvidado}>Entrar como convidado</Text>
+        <StatusBar style="auto" />
+        </View>
+      );
+}
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen/>
+      <Stack.Navigator initialRouteName='PaginaInicial'>
+        <Stack.Screen name='PaginaInicial' component={ PaginaInicial } />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#5da',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    image: {
+      width: 500,
+      height: 500,
+      resizeMode: 'stretch',
+    },
+    text: {
+      color: '#888',
+      clickable: true,
+    },
+  })
